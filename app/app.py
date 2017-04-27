d113b4f9794526530935e1cd8bea6739c426e825
Jady Urbina <jady.urbina@upr.edu>
2017-04-27 11:15:16 -0400

    Added Login Manager, User Class, and the about, edit info and home pages

--- app/app.py
+++ app/app.py
@@ -1,96 +1,117 @@
-import os
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, g, redirect, url_for, \
      render_template, flash
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+
 
 app = Flask(__name__)  # create the application instance :)
-app.config.from_object(__name__)  # load config from this file , app.py
+app.config.from_object(__name__)  # load config from this file, app.py
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./test.db'
+app.config['SECRET_KEY'] = 'SECRETKEY'
+db = SQLAlchemy(app)
 
 # Load default config and override config from an environment variable
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'app.db'),
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
-app.config.from_envvar('APP_SETTINGS', silent=True)
 
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    email = db.Column('email', db.String(50), unique=True, index=True)
+    password = db.Column('password', db.String(15))
+    registered_on = db.Column('registered_on', db.DateTime)
+    first_name = db.Column('first_name', db.String(15))
+    last_name = db.Column('last_name', db.String(15))
+
+    def __init__(self, email, password, first_name, last_name):
+        self.email = email
+        self.password = password
+        self.registered_on = datetime.utcnow()
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.first_name+self.last_name)
 
 
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
+@app.route('/user/edit')
+@login_required
+def edit_info():
+    # show the user profile for that user
+    return render_template('edit.html', user=current_user)
 
 
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
-
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """Closes the database again at the end of the request."""
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 @app.route('/')
-def show_entries():
-    db = get_db()
-    cur = db.execute('select title, text from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
+def home():
+    return render_template('home.html')
 
 
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted')
-    return redirect(url_for('show_entries'))
+@app.route('/<tweetId>')
+@login_required
+def tweet(tweetId):
+    return render_template('tweets.html', tweetId=tweetId)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
+    if request.method == 'GET':
+        return render_template('login.html')
+    email = request.form['email']
+    password = request.form['password']
+    registered_user = User.query.filter_by(email=email, password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid', 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('home'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['email'], request.form['password'], request.form['first_name'],
+                request.form['last_name'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
 
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
