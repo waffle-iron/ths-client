f3f4d212f916e00a42573efababb4ec4159d18c0
Diego Figueroa <diego.fv94@gmail.com>
2017-05-15 23:08:02 -0400

    optimize angular build - went with CDN sources for angular, jquery and ui-router as assets are not bundling correctly - implemented initial angular module thsClient with its controller - moved run method and bundling logic to init.py - removed GET server routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, bundles
+
+if __name__ == "__main__":
+    for bundle in bundles:
+        bundle.build()
+    app.run()
--- app/static/app.js
+++ app/static/app.js
@@ -1,36 +1,38 @@
-/**
- * Created by diegofigs on 5/11/17.
- */
-'use strict';
+(function() {
+    /**
+     * Created by diegofigs on 5/11/17.
+     */
+    'use strict';
 
-var app = angular.module('thsClient', [
-    'ui.router'
-]);
+    var app = angular.module('thsClient', [
+        'ui.router'
+    ]);
 
-app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
-    function($locationProvider, $stateProvider, $urlRouterProvider) {
-    // HTML5 Mode for compatibility with Flask
-    $locationProvider.html5Mode(true);
+    app.config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
+        function($locationProvider, $stateProvider, $urlRouterProvider) {
+            // HTML5 Mode for compatibility with Flask
+            $locationProvider.html5Mode(true);
 
-    // State declarations
-    $stateProvider
-        .state('app', {
-            abstract: true,
-            url: '/',
-            templateUrl: '/static/app.html',
-            controller: 'AppCtrl'
-        })
-        .state({
-            parent: 'app',
-            name: 'home',
-            url: '/home',
-            templateUrl: '/static/partials/home.html',
-            controller: 'AppCtrl'
-        });
+            // State declarations
+            $stateProvider
+                .state({
+                    name: 'app',
+                    url: '/',
+                    templateUrl: '/static/app.html',
+                    controller: 'AppCtrl'
+                })
+                .state({
+                    name: 'app.home',
+                    url: '/home',
+                    templateUrl: '/static/partials/home.html',
+                    controller: 'AppCtrl'
+                });
 
-    $urlRouterProvider.otherwise('/home');
-}]);
+            $urlRouterProvider.otherwise('/home');
+        }]);
 
-app.controller('AppCtrl', ['$scope', '$log', function($scope, $log) {
-    $log.log('Hello World from the App Controller using the $log Service');
-}]);
+    app.controller('AppCtrl', ['$scope', '$log', function($scope, $log) {
+        $log.log('Hello World from the App Controller using the $log Service');
+    }]);
+
+})();
